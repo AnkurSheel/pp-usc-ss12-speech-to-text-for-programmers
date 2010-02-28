66db739f84caf0971417d118713b679d090c8e60
mehta.dheeraj <mehta.dheeraj@a232caea-23d0-11df-9cb1-5138f70e336d>
2010-02-28 20:13:56 +0000

    DevStruct.java

---
+++ OurCode/src/ss12/s2t/DevStruct.java
@@ -0,0 +1,13 @@
+package ss12.s2t;
+
+public class DevStruct {
+	public String key;
+	public String value;
+	public DevStruct()
+	{}
+	public DevStruct(String key, String value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+}
